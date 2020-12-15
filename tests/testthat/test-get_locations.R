7804257e09da8ef1d0c82badd70eaa3cbbd9c16c
Amanda Dobbyn <amanda.e.dobbyn@gmail.com>
2020-12-15 15:16:40 -0500

    rm tests for now

--- tests/testthat/test-get_locations.R
+++
@@ -1,8 +0,0 @@
-test_that("get_locations() works", {
-  withr::local_options(list(meetupr.use_oauth = FALSE))
-  set_api_key("a good key")
-
-  pland <- get_locations("portland maine", api_key = "a good key")
-
-  expect_equal(pland$lat, "43.660000")
-})
